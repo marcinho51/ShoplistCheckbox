b80221c829567624be58896be8b13b3db905137e
Marcin <mwandalowski@wp.pl>
2020-01-08 22:57:33 +0100

    Updates

---
+++ precache-manifest.a213311585cb8c8a6c7b5bbb0e1e411a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb02ec039311e4a29b6b4f0b999d937",
+    "url": "/ShoplistCheckbox/index.html"
+  },
+  {
+    "revision": "70221c9080aad1123f71",
+    "url": "/ShoplistCheckbox/static/css/main.a90489cf.chunk.css"
+  },
+  {
+    "revision": "5c3c2fc67e2daed398e5",
+    "url": "/ShoplistCheckbox/static/js/2.5ee8621e.chunk.js"
+  },
+  {
+    "revision": "70221c9080aad1123f71",
+    "url": "/ShoplistCheckbox/static/js/main.89f15063.chunk.js"
+  },
+  {
+    "revision": "775268aae3f3d3038509",
+    "url": "/ShoplistCheckbox/static/js/runtime-main.d6213440.js"
+  },
+  {
+    "revision": "ad7d9facb25cde801cbd1ddadf54ddc2",
+    "url": "/ShoplistCheckbox/static/media/food.ad7d9fac.jpg"
+  },
+  {
+    "revision": "9af0a57cbcbb2fa05af9d99350fbfb99",
+    "url": "/ShoplistCheckbox/static/media/food2.9af0a57c.jpg"
+  },
+  {
+    "revision": "6e3518610276ee612d36d7d1af4133b9",
+    "url": "/ShoplistCheckbox/static/media/img1.6e351861.jpg"
+  }
+]);
